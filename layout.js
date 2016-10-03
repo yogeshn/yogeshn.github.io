5dd4b421e5dcc2a32c30f949599f7c5881402526
yogesh <yogesh.n998@gmail.com>
2016-10-02 23:06:03 -0500

    menu added

---
+++ layout.js
@@ -0,0 +1,19 @@
+document.addEventListener('DOMContentLoaded',function(){
+		$("body").jParticle({
+			background: "#14A989",
+			color: "#fff",particle: {
+	  color: "white",
+	  minSize: 2,
+	  maxSize: 8,
+	  speed: 80
+
+	} , particlesNumber: 70
+
+		});
+	},false);
+	
+	
+	
+	
+	
+	
