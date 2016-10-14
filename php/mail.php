6c692bb08c08e7b488135fcec8b379beeebbd66e
Yogesh Nitnaware <yogesh.n998@gmail.com>
2016-10-14 16:55:03 -0500

    Update mail.php

--- php/mail.php
+++ php/mail.php
@@ -9,10 +9,10 @@ if (!empty($_POST)){
   $_POST  = multiDimensionalArrayMap('cleanData', $_POST);
 
   //your email adress 
-  $emailTo ="yourmail@yoursite.com"; //"yourmail@yoursite.com";
+  $emailTo ="yogesh.n998@gmail.com; //"yourmail@yoursite.com";
 
   //from email adress
-  $emailFrom ="contact@yoursite.com"; //"contact@yoursite.com";
+  $emailFrom ="yogesh.n998@gmail.com"; //"contact@yoursite.com";
 
   //email subject
   $emailSubject = "Mail from Porta";
